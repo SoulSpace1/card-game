f1452096e34567474545571ec52806bc37ac90ca
svetlana <salvatore1864n@yandex.ru>
2022-11-30 00:07:10 +0300

    Заверстать игровой экран (2 и 3)

--- src/index.js
+++ src/index.js
@@ -1,37 +1,37 @@
 window.stateOfGame = {
-  level: null,
-  screen: 'start',
-  time: 0,
-  cards: null,
-  selectedCards: null,
+    level: null,
+    screen: 'start',
+    time: 0,
+    cards: null,
+    selectedCards: null,
 }
 
 function init() {
-  function startGame(event, form) {
-    event.preventDefault()
+    function startGame(event, form) {
+        event.preventDefault()
 
-    const formData = new FormData(form)
+        const formData = new FormData(form)
 
-    const level = formData.get('level')
+        const level = formData.get('level')
 
-    if (!level) {
-      alert('Выбери сложность!')
+        if (!level) {
+            alert('Выбери сложность!')
 
-      return
+            return
+        }
+
+        // alert(`выбран уровень ${level}`)
+        console.log(`выбран уровень ${level}`)
+
+        window.stateOfGame.level = level
     }
 
-    // alert(`выбран уровень ${level}`)
-    console.log(`выбран уровень ${level}`);
+    function initGame() {
+        const form = document.querySelector('.select-level__form')
+        form.addEventListener('submit', (event) => startGame(event, form))
+    }
 
-    window.stateOfGame.level = level
-  }
-
-  function initGame() {
-    const form = document.querySelector('.select-level__form')
-    form.addEventListener('submit', (event) => startGame(event, form))
-  }
-
-  initGame()
+    initGame()
 }
 
 window.addEventListener('DOMContentLoaded', init)
